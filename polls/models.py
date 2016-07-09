37810ec801778b070c14a868a82dbb96b6a290fb
codyowl <codyowl@gmail.com>
2016-07-10 00:21:59 +0530

    Models configured

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from mongoengine import *
 
-# Create your models here.
+class Choice(EmbeddedDocument):
+    choice_text = StringField(max_length=200)
+    votes = IntField(default=0)
+
+class Poll(Document):
+    question = StringField(max_length=200)
+    pub_date = DateTimeField(help_text='date published')
+    choices = ListField(EmbeddedDocumentField(Choice))
